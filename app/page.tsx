0f688b5eb778604c60b9e69faf7362cde27869d9
leonel Yimga <135525920+Leoneldev532@users.noreply.github.com>
2024-02-08 09:37:49 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -45,7 +45,11 @@ export default function Home() {
   // onLeaveBack
   return (
     <main className="flex min-h-screen flex-col items-center justify-between w-full">
-      <div className="min-h-screen p-16 text-black flex justify-start w-full leading-normal bg-green-400 items-center text-7xl "></div>
+      <div className="min-h-screen p-16 text-black flex justify-start w-full leading-normal bg-green-400 items-center text-7xl ">
+         <h2 className="txt-reveal">
+           Text Effect During scroll
+        </h2>
+      </div>
       <div className="min-h-screen p-16 text-black flex justify-start w-full leading-normal bg-red-400 items-center text-7xl ">
         <h2 className="txt-reveal">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque iure
